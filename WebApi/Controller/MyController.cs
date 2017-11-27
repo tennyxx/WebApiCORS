0cf14dd026025f53587734a0fad94924f0b9e2ee
tennyxx <liucx_wk@163.com>
2017-11-27 14:22:21 +0800

    添加
    cors解决跨域

---
+++ WebApi/Controller/MyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Filters;
+
+namespace WebApi.Controller
+{
+    public class MyController : ApiController
+    {
+        // GET: api/My
+        [ActionCORS]
+        public MyInfo GetMyInfo()
+        {
+            var entity = new MyInfo
+            {
+                Name = "liucx",
+                Age = 22,
+                Phone = "13011112222",
+                Remark = "测试跨域请求"
+            };
+            return entity;
+        }
+        public class MyInfo
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Phone { get; set; }
+            public string Remark { get; set; }
+        }
+
+    }
+}
---
+++ WebApi/Filters/ActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace WebApi.Filters
+{
+    public class ActionCORS:ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            //响应头添加Access-Control-Allow-Origin  *是全部允许，如果允许某个域名，则直接写域名
+            actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin","*");
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
